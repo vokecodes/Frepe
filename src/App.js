738d2d654afacc91ddc4cf25c495a4f95d60c8d7
voke <vefe44@gmail.com>
2019-01-18 00:53:30 +0100

    Add files

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './frepe.js'
+import Navbar from './components/navbar'
+import Banner from './components/banner'
+import About from './components/about'
+import Work from './components/work'
+import Contact from './components/contact'
+import Footer from './components/footer'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+
+      <React.Fragment>
+        <Navbar />
+        <Banner />
+        <About />
+        <Work />
+        <Contact />
+        <Footer />
+      </React.Fragment>
+      // <div className="App">
+      //   <header className="App-header">
+      //     <img src={logo} className="App-logo" alt="logo" />
+      //     <p>
+      //       Edit <code>src/App.js</code> and save to reload.
+      //     </p>
+      //     <a
+      //       className="App-link"
+      //       href="https://reactjs.org"
+      //       target="_blank"
+      //       rel="noopener noreferrer"
+      //     >
+      //       Learn React
+      //     </a>
+      //   </header>
+      // </div>
     );
   }
 }
